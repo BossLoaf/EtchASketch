d7fcc43b79720637ad2c218ad0bf67a5a4202496
BossLoaf <149402647+BossLoaf@users.noreply.github.com>
2024-03-14 10:00:58 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
+//Creates initial 16x16 Grid
 function makeGrid(sideLength) {
     let screen = document.querySelector("#sketchScreen");
     screen.replaceChildren();
-    let cellHeight = Math.floor(500/sideLength) + "px";
-    let cellWidth = Math.floor(500/sideLength) + "px";
+    let cellHeight = Math.floor(500 / sideLength) + "px";
+    let cellWidth = Math.floor(500 / sideLength) + "px";
     for (let i = 0; i < sideLength; i++) {
         let column = document.createElement("div");
         column.classList.add("column");
@@ -19,34 +20,49 @@ function makeGrid(sideLength) {
     }
 }
 
+function getRandomNumber(min, max) {
+    return Math.random() * (max - min) + min;
+}
+
+let currentColor = "red";
+
 let colorRed = function colorChange(e) {
-    e.target.style.backgroundColor = "red";
+    e.target.style.backgroundColor = currentColor;
 }
 
 let colorYellow = function colorChange(e) {
-    e.target.style.backgroundColor = "yellow";
+    e.target.style.backgroundColor = currentColor;
 }
 
 let colorBlue = function colorChange(e) {
-    e.target.style.backgroundColor = "blue";
+    e.target.style.backgroundColor = currentColor;
+}
+
+let colorRandom = function colorChange(e) {
+    e.target.style.backgroundColor = currentColor;
 }
 
 let buttonRed = document.querySelector("#red")
 buttonRed.onclick = function drawRed() {
-    let row = document.querySelector("div");
-    row.addEventListener("mouseover", colorRed);
+    currentColor = "red";
 }
 
 let buttonYellow = document.querySelector("#yellow")
 buttonYellow.onclick = function drawYellow() {
-    let row = document.querySelector("div");
-    row.addEventListener("mouseover", colorYellow);
+    currentColor = "yellow";
 }
 
 let buttonBlue = document.querySelector("#blue")
 buttonBlue.onclick = function drawBlue() {
-    let row = document.querySelector("div");
-    row.addEventListener("mouseover", colorBlue);
+    currentColor = "blue";
+}
+
+let buttonRandom = document.querySelector("#random")
+buttonRandom.onclick = function drawRandom() {
+    let randomOne = getRandomNumber(0, 256);
+    let randomTwo = getRandomNumber(0, 256);
+    let randomThree = getRandomNumber(0, 256);
+    currentColor = "rgb(" + randomOne + ", "  + randomTwo + ", " + randomThree + ")";
 }
 
 let button = document.querySelector("#createGrid");
